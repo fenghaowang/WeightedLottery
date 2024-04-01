f6033792eb5071d79f165b89fb7e0580ccd4752e
fenghaowang <fenghaowang@gmail.com>
2024-03-31 23:01:54 -0700

    Initial repository creation

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @author fenghaow
+ * @version 20240331
+ */
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
